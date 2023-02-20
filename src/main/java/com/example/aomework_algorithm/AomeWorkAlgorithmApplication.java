4d131ef99c2044fee5bdceb4809a97a3b02a37ef
vadimkosolapov <vilgutis@icloud.com>
2023-02-20 15:31:19 +0500

    Initial commit

---
+++ src/main/java/com/example/aomework_algorithm/AomeWorkAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.aomework_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AomeWorkAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AomeWorkAlgorithmApplication.class, args);
+    }
+
+}
